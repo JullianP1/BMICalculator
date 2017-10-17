d0ffd783432884283498a5d60f64194f2cfb3837
JP <JullianP1@learn.cssd.ab.ca>
2017-10-17 13:35:51 -0600

    created detailviewcontroller

---
+++ detailViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BMICalculator
+{
+    public partial class detailViewController : UIViewController
+    {
+        public detailViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
